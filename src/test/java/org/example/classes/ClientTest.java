8827e32e9534444d76d5377b46f6fd8b5f000105
Mihai <sarbumihai0@gmail.com>
2025-05-17 15:05:36 +0300

    Client test

---
+++ src/test/java/org/example/classes/ClientTest.java
@@ -0,0 +1,12 @@
+package org.example.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @Test
+    void achizitieProdus() {
+    }
+}
