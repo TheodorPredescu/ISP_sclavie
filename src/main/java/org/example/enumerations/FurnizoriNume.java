2ccc1ba79d74b20ce6d7ef30818cdc45a15ef222
bogdan624 <bogdan6241@gmail.com>
2025-05-17 15:59:40 +0300

    Am taiat din enumuri

--- src/main/java/org/example/enumerations/FurnizoriNume.java
+++ src/main/java/org/example/enumerations/FurnizoriNume.java
@@ -3,12 +3,9 @@ package org.example.enumerations;
 public enum FurnizoriNume {
     NESTLE,
     UNILEVER,
-    PROCTER_GAMBLE,
     PEPSICO,
     COCA_COLA,
     DANONE,
-    KRAFT_HEINZ,
     LIDL_SUPPLY,
-    CARREFOUR_SUPPLY,
     AMAZON_SUPPLY
 }
