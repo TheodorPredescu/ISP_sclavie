16f60f54bcfc8a7bc7c50b33c2ca250f6bc217ea
TheodorPredescu <predescutheodor@gmail.com>
2025-05-17 14:51:09 +0300

    adaugare testare

---
+++ src/test/java/org/example/classes/AdminTest.java
@@ -0,0 +1,24 @@
+package org.example.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+
+    @Test
+    void modificareReducereProdus() {
+    }
+
+    @Test
+    void confirmareComanda() {
+    }
+
+    @Test
+    void modificareComanda() {
+    }
+
+    @Test
+    void verificareStoc() {
+    }
+}
